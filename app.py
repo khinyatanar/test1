fabfdf624d616c69f3d3a41a79d14d62e014e678
khinyatanar <khinyatanaroo374@gmail.com>
2026-07-01 16:13:21 +0630

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
-st.title("Development of a Web-based")
-st.header("Pyay University")
-st.subheader("Physics")
-st.write("Enter your name")
-st.text_input("username",key="dcs_user")
-st.text_input("Password",type="password",key="dcs_pass")
+st.title('DCS Project')
+st.markdown("<h1 style='text-align: center;'>Online Examination System</h1>", unsafe_allow_html=True)
+left_col, middle_col, right_col = st.columns([1.5, 2, 1.5])
+with middle_col:
+    username = st.text_input("username", key="dcs_user")
+    password = st.text_input("Password", type="password", key="dcs_pass")
